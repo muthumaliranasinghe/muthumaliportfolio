35b3c10652fae2a60cd921eba4a1307f92cb76b4
muthumaliranasinghe <muthumaliranasinghe14@gmail.com>
2022-01-08 17:14:07 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,8 @@ const Home = () => {
             <section className="home-page">
                 <h1>Muthumali Ranasinghe</h1>
                 {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}
-                <p>My name is Muthumali Ranasinghe and I am undergraduate. and now i am following Bachelor of Software Engineering(OUSL).I have been developing web sites and applications and I am willing to learn and develop every new technology and new language.</p>
+                <p>Perseverance is the hard work you put in after you've exhausted the hard work you've previously put in.
+One can create a better world by realizing one's own potential and having faith in one's own abilities.</p>
 
                 <div className="download">
                     <NormalBtn url="https://drive.google.com/drive/folders/1-K58-thM1ICHR-98Gql4e3--Ux-_PR7u?usp=sharing" text="Download Resume" class="cv-btn"/>
